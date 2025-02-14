45329830d603a3667f2dc4cac1a61241d2ae1afe
LauraFontana01 <fontanameiolaura@hotmail.com>
2025-02-14 13:11:15 -0300

    ajustes

--- exercises/activity_001/c.py
+++ exercises/activity_001/c.py
@@ -19,12 +19,20 @@ def title():
 
 class Student:
     def __init__(self, a=0, b=0, c=0, d=0):
+        """
+        Initialize 4 grades
+        param_1: 1º grade
+        param_2: 2º grade
+        param_3: 3º grade
+        param_4: 4º grade
+        """
         self.a = a
         self.b = b
         self.c = c
         self.d = d
 
     def get_grade(self):
+        """Iterate method - get 4 grades"""
         while True:
             try:
                 print('=' * 80)
@@ -37,18 +45,29 @@ class Student:
                 self.d = float(input('Enter 4º grade: '))
                 print('=' * 80)
                 print()
-                break
-            except:
+                if all(
+                    0 <= grade <= 10 for grade in [
+                        self.a, self.b, self.c, self.d
+                        ]):
+                    break
+                else:
+                    print('-' * 80)
+                    print('Grades must be between 0-10. Try again.')
+                    print('=' * 80)
+                    print()
+            except ValueError:
                 print('-' * 80)
                 print('Invalid grade. Try again.')
                 print('=' * 80)
                 print()
 
     def calculate_media(self):
+        """Calculate method - Return media"""
         return (self.a + self.b + self.c + self.d) / 4
 
 
 def main():
+    """Main software"""
     cls_term()
     title()
 
