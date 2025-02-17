b77b9f57cfbaeac96972c9d813d8775f7a2f4503
LauraFontana01 <fontanameiolaura@hotmail.com>
2025-02-16 21:01:33 -0300

    exercise i - complete!

--- exercises/activity_001/i.py
+++ exercises/activity_001/i.py
@@ -1,6 +1,7 @@
 # STUDENT: BRUNO C. RODGERS
 # DATE: 14/02/2025
-# Faça um programa que receba um valor em reais, depois calcule quantos dólares daria para comprar com esse valor.
+# - Receive 1 value real;
+# - Calculate how many Dolars to buy with this amount.
 
 import os
 
@@ -11,19 +12,65 @@ def cls_term():
 
 def title():
     print('=' * 80)
-    print('POO - '.center(80))
+    print('POO - CAMBIO REAL TO DOLAR'.center(80))
     print('=' * 80)
     print()
 
 
+class Cambio:
+    def __init__(self, real=0):
+        """
+        Initialize 1 attribute
+        param_1: BR$
+        """
+        self.real = real
 
+    def get_value(self):
+        """
+        Iterate method - get value
+        """
+        while True:
+            try:
+                print('=' * 80)
+                self.real = float(input('Enter value: '))
+
+                if self.real > 0:
+                    print('=' * 80)
+                    print()
+                    break
+
+                else:
+                    print('-' * 80)
+                    print('Invalid value. Try again.')
+                    print('=' * 80)
+                    print()
+
+            except ValueError:
+                print('-' * 80)
+                print('Invalid input. Try again.')
+                print('=' * 80)
+                print()
+
+    def convert_value(self):
+        """
+        Calculate method - BR$ to Dolar
+        1: Return dolars
+        2: Return change
+        """
+        return self.real / 5.70
 
 
 def main():
     cls_term()
     title()
 
+    cambio = Cambio()
+    cambio.get_value()
 
+    print('=' * 80)
+    print(f'You can buy $ {cambio.convert_value():.2f} Dolars.')
+    print('=' * 80)
+    print()
 
 
 if __name__ == '__main__':
