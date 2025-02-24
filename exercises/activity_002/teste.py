177da9f39656b77526a4dee609e5a83538dcc257
LauraFontana01 <fontanameiolaura@hotmail.com>
2025-02-24 10:59:56 -0300

    teste

--- exercises/activity_002/teste.py
+++ exercises/activity_002/teste.py
@@ -1,5 +1,39 @@
-from packs.menu import cls_term, title, menu_1, menu_2
-from packs.classes import Car, Motorcycle, Truck, registered_vehicles
+import os
+
+
+def cls_term():
+    os.system('cls')
+
+
+def title():
+    print('=' * 80)
+    print('POO - VEHICLE REGISTER SYSTEM'.center(80))
+    print('=' * 80)
+    print()
+
+
+def menu_1():
+    print('=' * 80)
+    print('Menu:'.center(80))
+    print('=' * 80)
+    print('1. Register vehicle.')
+    print('2. Modify register.')
+    print('3. Delete register.')
+    print('4. Show registers.')
+    print('5. Exit.')
+    print('-' * 80)
+
+
+def menu_2():
+    print('=' * 80)
+    print('Especify Menu:'.center(80))
+    print('=' * 80)
+    print('1. Car.')
+    print('2. Motorcycle.')
+    print('3. Truck.')
+    print('4. Back.')
+    print('-' * 80)
+
 
 # Menu Principal
 def main_menu():
@@ -159,52 +193,6 @@ def register_car():
     return car.car_data() # Retornar método - dados carro
 
 
-def register_motorcycle():
-    """Function - register motorcycle
-
-    Returns:
-        method: return method motorcycle get data
-    """
-    motorcycle = Motorcycle() # Instanciar objeto - motocicleta
-
-    print('=' * 80)
-    while True:
-        try:
-            motorcycle.get_data() # Chamar método - receber dados motocicleta
-            break
-        except ValueError as e:
-            print('-' * 80)
-            print(e)
-            print('-' * 80)
-
-    print('=' * 80)
-    print('Motorcycle registered successfully.')
-    return motorcycle.motorcycle_data() # Retornar método - dados motocicleta
-
-
-def register_truck():
-    """Function - register truck
-
-    Returns:
-        method: return method truck get data
-    """
-    truck = Truck() # Instanciar objeto - caminhão
-
-    print('=' * 80)
-    while True:
-        try:
-            truck.get_data() # Chamar método - receber dados caminhão
-            break
-        except ValueError as e:
-            print('-' * 80)
-            print(e)
-            print('-' * 80)
-
-    print('=' * 80)
-    print('Truck registered successfully.')
-    return truck.truck_data() # Retornar método - dados caminhão
-
-
 def register_again():
     """Function - continue registering
 
@@ -311,86 +299,6 @@ def modify_car():
             input('Car code not found. Press enter to try again: ')
 
 
-def modify_motorcycle():
-    """Function - modify motorcycle data
-    """
-    while True:
-        cls_term()
-        title()
-        print('Modify Motorcycle Information:')
-        print('=' * 80)
-        print()
-
-        # Exibe todos as motocicletas registradas
-        for vehicle in registered_vehicles.values():
-
-            # Condicionar existência - motocicleta
-            if isinstance(vehicle, Motorcycle):
-                print('=' * 80)
-                print('Registered Motorcycles:')
-                vehicle.vehicle_data() # Exibir dados - motocicleta
-                print('=' * 80)
-                print()
-
-        print('=' * 80)
-        code = input('Enter mortorcycle code to modify: ').strip()
-        print('-' * 80)
-
-        # Condicionar existência - registro motocicleta
-        if code.isdigit() and int(code) in registered_vehicles:
-            motorcycle = registered_vehicles[int(code)] # Criar variável - receber registro existente
-
-            print(f'You selected: {motorcycle.vehicle_data()}')
-            motorcycle.modify_motorcycle() # Chamar método - modificar dados motocicleta
-            registered_vehicles[int(code)] = motorcycle # Substitui dados
-            print('=' * 80)
-            print('Motorcycle modified successfully.')
-            break
-        else:
-            print('-' * 80)
-            input('Motorcycle code not found. Press enter to try again: ')
-
-
-def modify_truck():
-    """Function - modify truck data
-    """
-    while True:
-        cls_term()
-        title()
-        print('Modify Truck Information:')
-        print('=' * 80)
-        print()
-
-        # Exibe todos os caminhões registrados
-        for vehicle in registered_vehicles.values():
-
-            # Condicionar existência - caminhão
-            if isinstance(vehicle, Truck):
-                print('=' * 80)
-                print('Registered Trucks:')
-                vehicle.vehicle_data() # Exibir dados - caminhão
-                print('=' * 80)
-                print()
-
-        print('=' * 80)
-        code = input('Enter car code to modify: ').strip()
-        print('-' * 80)
-
-        # Condicionar existência - registro caminhão
-        if code.isdigit() and int(code) in registered_vehicles:
-            truck = registered_vehicles[int(code)] # Criar variável - receber registro existente
-
-            print(f'You selected: {truck.vehicle_data()}')
-            truck.modify_truck() # Chamar método - modificar dados caminhão
-            registered_vehicles[int(code)] = truck # Substitui dados
-            print('=' * 80)
-            print('Truck modified successfully.')
-            break
-        else:
-            print('-' * 80)
-            input('Truck code not found. Press enter to try again: ')
-
-
 def modify_again():
     """Function - continue modifying
 
@@ -481,61 +389,322 @@ def del_car():
             input('Car code not found. Press enter to try again: ')
 
 
-def del_motorcycle():
-    """Function - delete motorcycle data
-    """
-    while True:
-        cls_term()
-        title()
-        print('Delete Motorcycle Register:')
+# Criar dicionário para armazenamento
+registered_vehicles = {}       
+
+
+# Definir superclasse
+class Vehicle:
+    # Inicializar método construtor
+    def __init__(self, code=0, brand='', model='', year=0, color=''):
+        """_summary_
+
+        Args:
+            code (int): vehicle code. Defaults to 0.
+            brand (str): vehicle brand. Defaults to ''.
+            model (str): vehicle model. Defaults to ''.
+            year (int): vehicle year. Defaults to 0.
+            color (str): vehicle color. Defaults to ''.
+        """
+        # Definir atributos privados
+        self._code = code
+        self._brand = brand
+        self._model = model
+        self._year = year
+        self._color = color
+
+    # Getter
+    @property
+    def code(self):
+        """Return private value
+
+        Returns:
+            int: vehicle code
+        """
+        return self._code # Retornar valor privado - código veículo
+
+    # Setter
+    @code.setter
+    def code(self, value):
+        """Receive value
+
+        Args:
+            value (int): vehicle code
+
+        Raises:
+            ValueError: error positive value
+            ValueError: error code already registered
+        """
+        # Retornar erro de validação
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError('Register code must is a positive number.')
+        if value in registered_vehicles:
+            raise ValueError('This code is already in use.')
+        self._code = value # Receber valor - código veículo
+        registered_vehicles[value] = self # Registrar valor - dicionário de registros
+
+    @property
+    def brand(self):
+        """Return private value
+
+        Returns:
+            string: vehicle brand
+        """
+        return self._brand # Retornar valor privado - marca veículo
+
+    @brand.setter
+    def brand(self, value):
+        """Receive value
+
+        Args:
+            value (string): vehicle brand
+
+        Raises:
+            ValueError: error empty input
+        """
+        # Retornar erro de validação
+        if not value.strip():
+            raise ValueError('Brand cannot be empty.')
+        self._brand = value.strip() # Receber valor - marca veículo
+
+    @property
+    def model(self):
+        """Return private value
+
+        Returns:
+            string: vehicle model
+        """
+        return self._model # Retornar valor privado
+
+    @model.setter
+    def model(self, value):
+        """Receive value
+
+        Args:
+            value (string): vehicle model
+
+        Raises:
+            ValueError: error empty input
+        """
+        # Retornar erro de validação
+        if not value.strip():
+            raise ValueError('Model cannot be empty.')
+        self._model = value.strip() # Receber valor - modelo veículo
+
+    @property
+    def year(self):
+        """Return private value
+
+        Returns:
+            int: vehicle year
+        """
+        return self._year # Retornar valor privado - modelo veículo
+
+    @year.setter
+    def year(self, value):
+        """Receive value
+
+        Args:
+            value (int): vehicle year
+
+        Raises:
+            ValueError: error positive value
+        """
+        # Retornar erro de validação
+        if not isinstance(value, int) or value <= 0:
+            raise ValueError('Year must be a positive value.')
+        self._year = value # Receber valor - ano veículo
+
+    @property
+    def color(self):
+        """Return private value
+
+        Returns:
+            string: vehicle color
+        """
+        return self._color # Retornar valor privado - cor veículo
+
+    @color.setter
+    def color(self, value):
+        """Receive value
+
+        Args:
+            value (string): vehicle color
+
+        Raises:
+            ValueError: error empty input
+        """
+        # Retornar erro de validação
+        if not value.strip():
+            raise ValueError('Color cannot be empty.')
+        self._color = value.strip() # Receber valor - cor veículo
+
+    # Método - receber dados veículo
+    def get_data(self):
+        """Method - receive vehicle data
+        """
+        while True:
+            try:
+                self.code = int(input('Enter code: '))
+                print('-' * 80)
+                break
+            except ValueError as e:
+                print('-' * 80)
+                print(e)
+                print('-' * 80)
+
+        while True:
+            try:
+                self.brand = input('Enter brand: ')
+                print('-' * 80)
+                break
+            except ValueError as e:
+                print('-' * 80)
+                print(e)
+                print('-' * 80)
+
+        while True:
+            try:
+                self.model = input('Enter model: ')
+                print('-' * 80)
+                break
+            except ValueError as e:
+                print('-' * 80)
+                print(e)
+                print('-' * 80)
+
+        while True:
+            try:
+                self.year = int(input('Enter year manufacture (Format: YYYY): '))
+                print('-' * 80)
+                break
+            except ValueError as e:
+                print('-' * 80)
+                print(e)
+                print('-' * 80)
+
+        while True:
+            try:
+                self.color = input('Enter color: ')
+                print('-' * 80)
+                break
+            except ValueError as e:
+                print('-' * 80)
+                print(e)
+                print('-' * 80)
+
+    # Método - modificar dados veículo
+    def modify_vehicle(self):
+        """Method - modify vehicle data
+        """
         print('=' * 80)
+        self.get_data()
 
-        # Exibir todas as motocicletas registradas
-        for vehicle in registered_vehicles.values():
+    # Método - exibir dados veículo
+    def vehicle_data(self):
+        """Method - show vehicle data
+        """
+        print(f'Code: {self.code}')
+        print(f'Brand: {self.brand}')
+        print(f'Model: {self.model}')
+        print(f'Year: {self.year}')
+        print(f'Color: {self.color}')
 
-            # Condicionar existência - motocicleta 
-            if isinstance(vehicle, Motorcycle):
-                vehicle.vehicle_data() # Exibir dados - motocicleta
+    # Método - criar dict veículo
+    def vehicle_dict(self):
+        """Create vehicle dict
 
+        Returns:
+            dict: vehicle dict
+        """
+        return {
+            'code': self.code,
+            'brand': self.brand,
+            'model': self.model,
+            'year': self.year,
+            'color': self.color
+        }
+
+
+# Definir classe derivada - Carro
+class Car(Vehicle):
+    # Inicializar método construtor
+    def __init__(self, code=0, brand='', model='', year=0, color='', fuel_type=''):
+        """Initialize especific attributes
+
+        Args:
+            code (int): car code. Defaults to 0.
+            brand (str): car brand. Defaults to ''.
+            model (strl): car model. Defaults to ''.
+            year (int): car year. Defaults to 0.
+            color (str): car color. Defaults to ''.
+            fuel_type (str): car fuel type. Defaults to ''.
+        """
+        super().__init__(code, brand, model, year, color) # Herdar atributos - Veículo
+        self._fuel_type = fuel_type # Definir atributo privado
+
+    @property
+    def fuel_type(self):
+        """Return private value
+
+        Returns:
+            string: car fuel type
+        """
+        return self._fuel_type # Retorna valor privado
+
+    @fuel_type.setter
+    def fuel_type(self, value):
+        """Receive value
+
+        Args:
+            value (str): car fuel type
+
+        Raises:
+            ValueError: error empty input
+        """
+        # Retornar erro de validação
+        if not value.strip():
+            raise ValueError('Fuel type cannot be empty.')
+        self._fuel_type = value.strip() # Receber valor - tipo de combustível carro
+
+    # Método - receber dados carro
+    def get_data(self):
+        """Method - receive car data
+        """
+        super().get_data() # Herdar método - receber dados veículo
+        while True:
+            try:
+                self.fuel_type = input('Enter fuel type: ')
+                print('=' * 80)
+                print()
+                break
+            except ValueError as e:
+                print('-' * 80)
+                print(e)
+                print('-' * 80)
+
+    # Método - modificar dados carro
+    def modify_car(self):
+        """Method - modify car data
+        """
+        self.get_data()
+
+    # Método - exibir dados carro
+    def car_data(self):
+        """Method - show car data
+        """
+        super().vehicle_data() # Herdar método - exibir dados veículo
+        print(f'Fuel type: {self.fuel_type}')
         print('=' * 80)
-        code = input('Enter motorcycle code to delete: ').strip()
-        print('-' * 80)
+        print()
 
-        # Condicionar existência - registro motocicleta
-        if code.isdigit() and int(code) in registered_vehicles:
-            del_motorcycle = registered_vehicles.pop(int(code)) # Remover motocicleta
-            print(f'Motorcycle (Code: {del_motorcycle.code}) deleted successfully.')
-            break
-        else:
-            print('-' * 80)
-            input('Motorcycle code not found. Press enter to try again: ')
+    # Método - criar dict carro
+    def car_dict(self):
+        """Method - create car dict
 
-
-def del_truck():
-    """Function - delete truck data
-    """
-    while True:
-        cls_term()
-        title()
-        print('Delete Truck Register:')
-        print('=' * 80)
-
-        # Exibir todos os caminhões registrados
-        for vehicle in registered_vehicles.values():
-
-            # Condicionar existência - caminhão 
-            if isinstance(vehicle, Truck):
-                vehicle.vehicle_data() # Exibir dados - caminhão
-
-        print('=' * 80)
-        code = input('Enter truck code to delete: ').strip()
-        print('-' * 80)
-
-        # Condicionar existência - registro caminhão
-        if code.isdigit() and int(code) in registered_vehicles:
-            del_truck = registered_vehicles.pop(int(code)) # Remover caminhão
-            print(f'Truck (Code: {del_truck.code}) deleted successfully.')
-            break
-        else:
-            print('-' * 80)
-            input('Truck code not found. Press enter to try again: ')
+        Returns:
+            dict: car dict
+        """
+        car_dict = super().vehicle_dict() # Herdar método - véiculo dict
+        car_dict['fuel_type'] = self.fuel_type # Adicionar registro - tipo de combustível carro
+        return car_dict # Retornar dict - carro
